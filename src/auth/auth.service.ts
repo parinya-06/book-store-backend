49b1a2c96736af804fbec3fbe00246706ed93996
parinya-06 <parinya.ru.61@ubu.ac.th>
2022-07-13 13:48:34 +0700

    fix update refreshToken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common'
-import { UsersService } from '../users/users.service'
 import { JwtService } from '@nestjs/jwt'
-import loginDto from './dto/login.dto'
 import * as bcrypt from 'bcrypt'
+import { UsersService } from '../users/users.service'
 
 @Injectable()
 export class AuthService {
@@ -11,11 +10,10 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findUsers(username)
-    let checkPassword
-    user.forEach((value: loginDto) => (checkPassword = value.password))
-    const isMatch = await bcrypt.compare(pass, checkPassword)
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findByUsername(username)
+    const [{ password: checkPassword }] = user
+    const isMatch = await bcrypt.compare(password, checkPassword)
     if (user && isMatch) {
       const [{ password, ...result }] = user
       return result
@@ -25,8 +23,25 @@ export class AuthService {
 
   async login(user: any) {
     const payload = { username: user.username, sub: user._id }
+    const tokens = await this.getTokens(payload)
+    await this.updateRefreshToken(user._id, tokens.refreshToken)
+    return tokens
+  }
+
+  async updateRefreshToken(userId: string, refreshToken: string) {
+    await this.usersService.update(userId, {
+      refreshToken: refreshToken,
+    })
+  }
+
+  async getTokens(payload: object) {
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(payload),
+      this.jwtService.signAsync(payload, { expiresIn: '7d' }),
+    ])
     return {
-      access_token: this.jwtService.sign(payload),
+      accessToken,
+      refreshToken,
     }
   }
 }
--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,3 +1,3 @@
 export const jwtConstants = {
-  secret: 'secretKey',
+  secret: String(process.env.SECRET_JWT),
 }
