73f132bdced4913d355bca1d74703b21316e1bff
parinya-06 <parinya.ru.61@ubu.ac.th>
2022-07-18 11:38:09 +0700

    fix register and validate pipe

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { ApiBody, ApiCreatedResponse, ApiTags } from '@nestjs/swagger'
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common'
+import bcrypt from 'bcrypt'
 
-import { LocalAuthGuard } from './guards/local-auth.guard'
-import { AuthService } from './auth.service'
 import LoginDTO from './dto/login.dto'
+import { AuthService } from './auth.service'
+import { LocalAuthGuard } from './guards/local-auth.guard'
+
+import { User } from '../users/schemas/user.schema'
+import CreateUserDTO from '../users/dto/create-user.dto'
+import { RegisterValidationPipe } from '../pipes/register-validation.pipe'
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private configService: ConfigService,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
@@ -19,5 +28,19 @@ export class AuthController {
   })
   async login(@Request() req) {
     return this.authService.login(req.user)
+  }
+
+  //เพิ่มข้อมูลสมาชิก
+  @Post('register')
+  async create(
+    @Body(RegisterValidationPipe) createUserDTO: CreateUserDTO,
+  ): Promise<User> {
+    const saltOrRounds = this.configService.get('saltOrRounds')
+    const { password } = createUserDTO
+    const hashedPassword = await bcrypt.hash(password, saltOrRounds)
+    return this.authService.create({
+      ...createUserDTO,
+      password: hashedPassword,
+    })
   }
 }
