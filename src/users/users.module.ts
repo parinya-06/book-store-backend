7fbefa535f01ca2cd3cde282cbabfd01ebf4948f
parinya-06 <parinya.ru.61@ubu.ac.th>
2022-07-18 11:37:34 +0700

    fix import

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module'
+
 import { UsersService } from './users.service'
-import { User, UserSchema } from './schemas/user.schema'
 import { UsersController } from './users.controller'
+import { User, UserSchema } from './schemas/user.schema'
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   providers: [UsersService],
-  exports: [UsersService],
   controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}
