71625ad957ee47083bf01b15a3bd46947cb3b558
parinya-06 <parinya.ru.61@ubu.ac.th>
2022-07-20 13:28:53 +0700

    fix update add ApiProperty and add status schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,5 +1,7 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 import { Document } from 'mongoose'
+
 import { ERole } from '../enums/enum-role'
 
 export type UserDocument = User & Document
@@ -9,6 +11,12 @@ export type UserDocument = User & Document
   versionKey: false,
 })
 export class User {
+  @ApiProperty({
+    description: 'The username Length 6-15 char',
+    type: String,
+    required: true,
+    example: 'test001',
+  })
   @Prop({
     index: true,
     unique: true,
@@ -17,6 +25,12 @@ export class User {
   })
   username: string
 
+  @ApiProperty({
+    description: 'The password Length 8-32 char',
+    type: String,
+    required: true,
+    example: 'testing001',
+  })
   @Prop({
     index: true,
     type: String,
@@ -24,6 +38,12 @@ export class User {
   })
   password: string
 
+  @ApiProperty({
+    description: 'The firstname Length than 1 char',
+    type: String,
+    required: true,
+    example: 'test',
+  })
   @Prop({
     index: true,
     type: String,
@@ -31,6 +51,11 @@ export class User {
   })
   firstname: string
 
+  @ApiProperty({
+    type: String,
+    required: false,
+    example: '001',
+  })
   @Prop({
     index: true,
     type: String,
@@ -39,17 +64,31 @@ export class User {
   })
   lastname?: string
 
+  @ApiProperty({
+    type: String,
+    example: 'user',
+  })
   @Prop({
     type: String,
     default: ERole.User,
   })
   roles?: ERole
 
+  @ApiProperty({
+    type: Boolean,
+    example: false,
+  })
   @Prop({
     type: Boolean,
     default: true,
   })
   enabled?: boolean
+
+  @Prop({
+    type: Boolean,
+    default: true,
+  })
+  status?: boolean
 }
 
 export const UserSchema = SchemaFactory.createForClass(User)
