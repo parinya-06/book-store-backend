6a107d1a82dba78c11cba53d0b6c6c3c79ef748c
parinya-06 <parinya.ru.61@ubu.ac.th>
2022-07-20 13:27:09 +0700

    fix update del required ApiProperty

--- src/users/entities/update-enable-user.entity.ts
+++ src/users/entities/update-enable-user.entity.ts
@@ -3,7 +3,6 @@ import { ApiProperty } from '@nestjs/swagger'
 export class UpdateEnableUserEntity {
   @ApiProperty({
     type: String,
-    required: true,
     example: false,
   })
   enabled: boolean
